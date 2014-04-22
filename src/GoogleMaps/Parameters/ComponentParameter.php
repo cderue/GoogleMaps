9cb7e367361b575ae827abbea2045d449c4721af
fabio <fabio@prod23>
2014-04-22 19:18:22 -0300

    charset

--- src/GoogleMaps/Parameters/ComponentParameter.php
+++ src/GoogleMaps/Parameters/ComponentParameter.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of Geoxygen
  *
- * (c) 2012 C�dric DERUE <cedric.derue@gmail.com>
+ * (c) 2012 Cédric DERUE <cedric.derue@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
